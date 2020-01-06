3e5334715b068f48ca987d7fddfb2c065484e606
DELL <ranjan.summit@gmail.com>
2020-01-06 00:25:26 -0800

    new activity added

---
+++ app/src/main/java/com/agatsa/testsdknew/userlogin/UserLogIn.java
@@ -0,0 +1,16 @@
+package com.agatsa.testsdknew.userlogin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.agatsa.testsdknew.R;
+
+public class UserLogIn extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_log_in);
+    }
+}
