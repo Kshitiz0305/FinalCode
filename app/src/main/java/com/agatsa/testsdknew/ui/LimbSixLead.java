c0b798668af91d51348176bd24db8f89edf3435a
kshitizbhandari <kshitizbhandari23@gmail.com>
2020-01-14 17:16:00 +0545

    showhide managed

--- app/src/main/java/com/agatsa/testsdknew/ui/LimbSixLead.java
+++ app/src/main/java/com/agatsa/testsdknew/ui/LimbSixLead.java
@@ -29,6 +29,18 @@ import com.agatsa.testsdknew.Models.ECGReport;
 import com.agatsa.testsdknew.R;
 import com.ontbee.legacyforks.cn.pedant.SweetAlert.SweetAlertDialog;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.StringWriter;
+import java.io.Writer;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import pl.droidsonroids.gif.GifImageView;
+
 public class LimbSixLead extends AppCompatActivity {
     private static final String CLIENT_ID = "5a3b4c16b4a56b000132f5d5b4580266565440bda51dcb4122d39844";
     private static final String SECRET_ID = "5a3b4c16b4a56b000132f5d5746be305d56c49e49cc88b12ccb05d71";
@@ -42,6 +54,16 @@ public class LimbSixLead extends AppCompatActivity {
     ECGReport ecgReport=new ECGReport();
     SweetAlertDialog pDialog;
     Toolbar toolbar;
+    TextView description;
+    public  static  final String imagesrc = "gif_lead1";
+    public  static  final String descriptionsrc = "ecginfo";
+    GifImageView gifImageView;
+    ArrayList<String> buttoncollectionshide = new ArrayList<String>(Arrays.asList("txtlimbleadone","txtlimboneagain","txtlimbleadtwo","txtlimbtwoagain","txtlimbagain","ll_savereport","ll_report","ll_complete"));
+    ArrayList<String> buttoncollectionsshow=new ArrayList<String>(Arrays.asList("txtlimbleadone","txtlimboneagain"));
+
+
+
+
 
 
 
@@ -61,10 +83,40 @@ public class LimbSixLead extends AppCompatActivity {
         setSupportActionBar(toolbar);
         getSupportActionBar().setTitle("Limb Six Lead");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-
+        hideAndSeek(buttoncollectionshide,true);
+        hideAndSeek(buttoncollectionsshow,false);
 
         initViews();
         initOnClickListener();
+    }
+
+    public void hideAndSeek(ArrayList<String> idsarray,boolean ishide){
+
+        if(ishide){
+            for(String selectedString:idsarray){
+
+                LinearLayout linearLayout = findViewById(getResourceId(selectedString,"id",getPackageName()));
+                 linearLayout.setVisibility(View.GONE);
+
+            }
+
+
+
+
+
+        }
+        else {
+
+            for(String selectedString:idsarray){
+
+                LinearLayout linearLayout = findViewById(getResourceId(selectedString,"id",getPackageName()));
+                linearLayout.setVisibility(View.VISIBLE);
+
+            }
+
+
+        }
+
     }
 
 
@@ -73,13 +125,29 @@ public class LimbSixLead extends AppCompatActivity {
         txtlimbleadone = findViewById(R.id.txtlimbleadone);
         savell=findViewById(R.id.savell);
         txtlimbleadtwo = findViewById(R.id.txtlimbleadtwo);
-        btnlimbHistory = findViewById(R.id.btnlimbHistory);
+
         completetaskll=findViewById(R.id.completetaskll);
         txtlimboneagain=findViewById(R.id.txtlimboneagain);
         txtlimbtwoagain=findViewById(R.id.txtlimbtwoagain);
         txtlimbagain=findViewById(R.id.txtlimbagain);
+        gifImageView=findViewById(R.id.gif_holder);
+        description=findViewById(R.id.tv_description);
+
+        showDynamicimage(imagesrc);
+        showDynamicDescription(descriptionsrc);
 
 
+
+
+    }
+
+    public void showDynamicDescription(String ecginfo){
+        description.setText(getResourceId(ecginfo,"string",getPackageName()));
+    }
+
+    public void showDynamicimage(String imagesrcs){
+
+        gifImageView.setImageResource((getResourceId(imagesrcs,"drawable",getPackageName())));
 
     }
 
@@ -100,11 +168,11 @@ public class LimbSixLead extends AppCompatActivity {
 
      });
 
-        btnlimbHistory.setOnClickListener(view -> {
-            Intent intent = new Intent(getApplicationContext(), HistoryActivity.class);
-            intent.putExtra("type", EcgTypes.NORMAL_ECG);
-            startActivity(intent);
-        });
+//        btnlimbHistory.setOnClickListener(view -> {
+//            Intent intent = new Intent(getApplicationContext(), HistoryActivity.class);
+//            intent.putExtra("type", EcgTypes.NORMAL_ECG);
+//            startActivity(intent);
+//        });
 
 
         pDialog = new SweetAlertDialog(this, SweetAlertDialog.PROGRESS_TYPE);
@@ -214,5 +282,50 @@ public class LimbSixLead extends AppCompatActivity {
         startActivity(intent);
 
     }
+
+
+//    public String getJsonStringfromFile(String jsonFileName, String resources, String packagename) {
+//
+//        InputStream is = getResources().openRawResource(getResourceId(jsonFileName,resources,packagename));
+//        Writer writer = new StringWriter();
+//        char[] buffer = new char[1024];
+//        try {
+//            Reader reader;
+//            reader = new BufferedReader(new InputStreamReader(is, "UTF-8"));
+//            int n;
+//            while ((n = reader.read(buffer)) != -1) {
+//                writer.write(buffer, 0, n);
+//            }
+//
+//            is.close();
+//        }
+//        catch (Exception e){
+//
+//
+//
+//        }
+//
+//
+//        finally {
+//
+//        }
+//
+//        String jsonString = writer.toString();
+//        return  jsonString;
+//    }
+    public  int getResourceId(String pVariableName, String pResourcename, String pPackageName)
+    {
+        try {
+            Log.d("rantestid",String.valueOf(getResources().getIdentifier(pVariableName, pResourcename, pPackageName)));
+            return getResources().getIdentifier(pVariableName, pResourcename, pPackageName);
+        } catch (Exception e) {
+            Log.d("rantest",e.getLocalizedMessage());
+            e.printStackTrace();
+            return -1;
+        }
+
+    }
+
+
 
 }
